b075a3d1ee8814ac8bb4b40e8b5cab38e4369618
arky2131074 <120645642+arky2131074@users.noreply.github.com>
2023-01-01 02:13:16 +0700

    Add files via upload

---
+++ Clownfish.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AQUARIUM;
+
+public class Clownfish extends Ikan {
+    public Clownfish (){
+        this.nama = "Clown Fish";
+        this.kolam = "Bersih";
+        this.lapar = "Lapar";
+        this.makan = "";
+    }
+    
+}
---
+++ Platy.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AQUARIUM;
+
+public class Platy extends Ikan {
+   public Platy (){
+        this.nama = "Platy Fish";
+        this.kolam = "Bersih";
+        this.lapar = "Lapar";
+        this.makan = "";
+    }
+}
